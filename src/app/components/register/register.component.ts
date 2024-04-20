6b1161a9a9fb55d6045420214127bc34c1894970
Trong <jr16022002@gmail.com>
2024-04-20 23:38:19 +0700

    update signature-dish

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -26,13 +26,13 @@ export class RegisterComponent {
   protected confirmPassword: FormControl = new FormControl(null, [Validators.required]);
   protected agreement: FormControl = new FormControl(false);
 
-  // public registerForm: FormGroup = new FormGroup({
-  //   displayName: this.displayName,
-  //   username: this.username,
-  //   password: this.password,
-  //   confirmPassword: this.confirmPassword,
-  //   agreement: this.agreement
-  // }, [this.formGroupMatchValidator('confirmPassword', 'password')]);
+  public registerForm: FormGroup = new FormGroup({
+    displayName: this.displayName,
+    username: this.username,
+    password: this.password,
+    confirmPassword: this.confirmPassword,
+    agreement: this.agreement
+  }, [this.formGroupMatchValidator('confirmPassword', 'password')]);
 
 
   public matchValidator (controlToMatch: AbstractControl<any, any>): ValidatorFn
@@ -53,25 +53,29 @@ export class RegisterComponent {
     }
   }
 
-  public registerForm !: FormGroup;
-  constructor (private formBuilder: FormBuilder, private http: HttpClient, private router: Router){}
-  ngOnInit():void {
-    this.registerForm = this.formBuilder.group({
-      displayName: [''],
-      username: [''],
-      password: '',
-      confirmPassword: [''],
-      agreement: Boolean,
-    })
+  register(){
+
   }
 
-  register(){
-    this.http.post<any>("http://localhost:3000/registerUser", this.registerForm.value)
-    .subscribe( res => {
-      alert("Successfully registered");
-      this.registerForm.reset();
-      this.router.navigate(['login']);
-    })
-  }
+  // public registerForm !: FormGroup;
+  // constructor (private formBuilder: FormBuilder, private http: HttpClient, private router: Router){}
+  // ngOnInit():void {
+  //   this.registerForm = this.formBuilder.group({
+  //     displayName: [''],
+  //     username: [''],
+  //     password: '',
+  //     confirmPassword: [''],
+  //     agreement: Boolean,
+  //   })
+  // }
+
+  // register(){
+  //   this.http.post<any>("http://localhost:3000/registerUser", this.registerForm.value)
+  //   .subscribe( res => {
+  //     alert("Successfully registered");
+  //     this.registerForm.reset();
+  //     this.router.navigate(['login']);
+  //   })
+  // }
 
 }
--- src/app/components/signature-dish/signature-dish.component.ts
+++ src/app/components/signature-dish/signature-dish.component.ts
@@ -6,31 +6,5 @@ import { Component } from '@angular/core';
   styleUrl: './signature-dish.component.css'
 })
 export class SignatureDishComponent {
-  items: Array<{ imageUrl: string, title: string, description: string }> = [
-    // Danh sách các item bạn muốn hiển thị
-    {
-      imageUrl: 'https://food-cms.grab.com/compressed_webp/merchants/5-C3TYAU51EYM1JA/hero/398ef14332a84fa3b34034f753dea761_1691032150229147318.webp',
-      title: 'Item 1',
-      description: 'Mô tả item 1'
-    },
-    {
-      imageUrl: 'https://food-cms.grab.com/compressed_webp/merchants/5-C3TYAU51EYM1JA/hero/398ef14332a84fa3b34034f753dea761_1691032150229147318.webp',
-      title: 'Item 2',
-      description: 'Mô tả item 2'
-    },
-    // Thêm các item khác vào đây...
-  ];
 
-  ngOnInit(): void {
-    // Sắp xếp lại các item ngẫu nhiên
-    this.items = this.shuffleArray(this.items);
-  }
-
-  shuffleArray(array: Array<any>): Array<any> {
-    for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]]; // Hoán đổi các item
-    }
-    return array;
-  }
 }
