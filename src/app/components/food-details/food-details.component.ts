8291729ee5ba17d947a90e22168b8d4695f22c9c
Trong <jr16022002@gmail.com>
2024-05-26 22:50:18 +0700

    update 26/5

--- src/app/components/food-details/food-details.component.ts
+++ src/app/components/food-details/food-details.component.ts
@@ -9,12 +9,12 @@ import { HttpService } from '../../services/http.service';
 @Component({
   selector: 'app-food-details',
   templateUrl: './food-details.component.html',
-  styleUrl: './food-details.component.css'
+  styleUrl: './food-details.component.css',
 })
 export class FoodDetailsComponent implements OnInit {
   food: Food[] = [];
   page: number = 1;
-  itemsPerPage: number = 8;
+  itemsPerPage: number = 4;
   totalFood: any;
   foodCategory!: string | null;
   foodName!: string | null;
@@ -24,24 +24,28 @@ export class FoodDetailsComponent implements OnInit {
     private cartService: CartService,
     private activatedRoute: ActivatedRoute,
     private alertService: AlertService,
-    private httpService: HttpService,
+    private httpService: HttpService
   ) {
-    activatedRoute.paramMap.subscribe(params => {
+    activatedRoute.paramMap.subscribe((params) => {
       this.foodCategory = params.get('id');
       if (this.foodCategory) {
-        this.httpService.getFoodByCategory(this.foodCategory).subscribe((res: Food[]) => {
-          this.food = res
-        })
-      };
+        this.httpService
+          .getFoodByCategory(this.foodCategory)
+          .subscribe((res: Food[]) => {
+            this.food = res;
+          });
+      }
     });
 
-    activatedRoute.queryParamMap.subscribe(params => {
+    activatedRoute.queryParamMap.subscribe((params) => {
       this.foodName = params.get('name');
       if (this.foodName) {
-        this.httpService.searchFoodByName(this.foodName).subscribe((res: Food[]) => {
-          this.food = res
-        })
-      };
+        this.httpService
+          .searchFoodByName(this.foodName)
+          .subscribe((res: Food[]) => {
+            this.food = res;
+          });
+      }
     });
   }
 
@@ -51,14 +55,19 @@ export class FoodDetailsComponent implements OnInit {
 
   getAll() {
     this.foodService.getAll().subscribe((res: any) => {
-      this.food = res
-    })
+      this.food = res;
+    });
   }
 
   addToCart(item: Food) {
     console.log(item);
 
     this.cartService.addToCart(item);
-    this.alertService.appendAlert('Thêm vào giỏ hàng thành công', AlertType.success, 30, 'alert-container');
+    this.alertService.appendAlert(
+      'Thêm vào giỏ hàng thành công',
+      AlertType.success,
+      30,
+      'alert-container'
+    );
   }
 }
